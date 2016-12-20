25fc6c05d23c5c4d562abdc09aefeade7ce5c9f4
Damian Simon Peter <simon.peter@andela.com>
2016-12-20 20:23:26 +0100

    Created a helper module to house shared behaviours

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,13 @@ require 'flutterwave'
 require 'minitest/autorun'
 require 'webmock/minitest'
 require 'faker'
+
+module FlutterWaveTestHelper
+  def stub_flutterwave
+    stub_request(
+      :post, "#{Flutterwave::Utils::Constants::BASE_URL}"\
+      "#{url}"
+    ).to_return(status: 200, body: response_data.to_json)
+  end
+end
+
